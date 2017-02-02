379946a3c6ff61f423bf28e7b4c6dabb531e0946
Xinru Yan <yanxx418@d.umn.edu>
2017-02-01 20:59:08 -0600

    modified runit.sh

--- runit.sh
+++ runit.sh
@@ -1,8 +1,15 @@
 # echo 'Make sure to install kenlm for example: pip install https://github.com/kpu/kenlm/archive/master.zip'
-pip install https://github.com/kpu/kenlm/archive/master.zip
+#echo 'Installing kenlm'
+#pip install https://github.com/kpu/kenlm/archive/master.zip
+echo 'Getting training data ready'
 python avocado.py lm_train_dir/lm_train_data plain.txt
+echo 'Training languge model'
 kenlm-master/build/bin/lmplz -o 3 -S 70% < plain.txt > text.arpa
+echo 'Getting language model scores'
 python beets.py evaluation_dir/evaluation_data/ lm_train_result/lm_score/
+echo 'Getting result for task a'
+python cilantro_a.py lm_train_result/lm_score/
+echo 'Getting result for task b'
 python cilantro_b.py lm_train_result/lm_score/
 
 # echo 'Now please nevigate to your kenlm dirctory and run following commands to complie and train the languge model'
