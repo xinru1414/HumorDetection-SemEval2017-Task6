8aec4e93573a1f34f17f9d8c1040940e43a9ce3e
yanxx418 <yanxx418@d.umn.edu>
2017-02-28 14:36:40 -0600

    Delete rm_runit.sh

--- rm_runit.sh
+++
@@ -1,6 +0,0 @@
-# This script sets baseline for both tasks.
-
-echo 'baseline for task A'
-python src/random_gen_a.py mydata/lm_train_result/lm_score/
-echo 'baseline for task B'
-python src/random_gen_b.py mydata/lm_train_result/lm_score
