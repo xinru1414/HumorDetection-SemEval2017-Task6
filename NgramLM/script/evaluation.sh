4242391e418507cedd33eb6a57dd203a04ebff16
xinru1414 <yanxx418@d.umn.edu>
2018-01-24 10:25:36 -0600

    update script to match new organization

--- NgramLM/script/evaluation.sh
+++ NgramLM/script/evaluation.sh
@@ -1,8 +1,8 @@
 echo 'Evaluating Task A (with news training data)'
-python code/TaskA_Eval/TaskA_Eval_Script.py mydata/lm_news_result/A_lm/ data/gold_data/
+python ../../Evaluation/TaskA_Eval/TaskA_Eval_Script.py ../mydata/lm_news_result/A_lm/ ../../data/gold_data/
 echo 'Evaluating Task B (with news training data)'
-python code/TaskB_Eval/TaskB_Eval_Script.py mydata/lm_news_result/B_lm/ data/gold_data/
+python ../../Evaluation/TaskB_Eval/TaskB_Eval_Script.py ../mydata/lm_news_result/B_lm/ ../../data/gold_data/
 echo 'Evaluating Task A (with tweets training data)'
-python code/TaskA_Eval/TaskA_Eval_Script.py mydata/lm_train_result/A_lm/ data/gold_data/
+python ../../Evaluation/TaskA_Eval/TaskA_Eval_Script.py ../mydata/lm_train_result/A_lm/ ../../data/gold_data/
 echo 'Evaluating Task B (with tweets training data)'
-python code/TaskB_Eval/TaskB_Eval_Script.py mydata/lm_train_result/B_lm/ data/gold_data/
+python ../../Evaluation/TaskB_Eval/TaskB_Eval_Script.py ../mydata/lm_train_result/B_lm/ ../../data/gold_data/
