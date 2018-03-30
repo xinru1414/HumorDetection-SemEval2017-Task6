145ae5471660a6491f29cc946454e1f1a963409a
Yan Xinru <yanxx418@d.umn.edu>
2018-03-29 19:16:06 -0500

    ngram

--- NgramLM/src/pre_process.py
+++ NgramLM/src/pre_process.py
@@ -1,14 +1,17 @@
 import preprocessor as p
 import sys, os
+import re
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
 def main():
+	with open(infile, "r", encoding='UTF-8') as f:
+		lines = f.readlines()
 	with open(outfile, 'w') as o:
-		with open(infile, "r", encoding='UTF-8') as f:
-			for line in f:
-				o.write(line.lower())
+		for line in lines:
+			s = re.sub('(?<! )(?=[.,!?()])|(?<=[.,!?()])(?! )', r' ', line)
+			o.write(s)
 
 if __name__ == '__main__':
     main()
---
+++ NgramLM/src/run.sh
@@ -0,0 +1,7 @@
+python3 pre_process.py ~/Developer/Thesis/SemEval2017/Humor/AD/1MBnew.txt ~/Developer/Thesis/SemEval2017/Humor/AD/new1MB.txt
+python3 pre_process.py ~/Developer/Thesis/SemEval2017/Humor/AD/25MBnew.txt ~/Developer/Thesis/SemEval2017/Humor/AD/new25MB.txt
+python3 pre_process.py ~/Developer/Thesis/SemEval2017/Humor/AD/50MBnew.txt ~/Developer/Thesis/SemEval2017/Humor/AD/new50MB.txt
+python3 pre_process.py ~/Developer/Thesis/SemEval2017/Humor/AD/200MBnew.txt ~/Developer/Thesis/SemEval2017/Humor/AD/new200MB.txt
+python3 pre_process.py ~/Developer/Thesis/SemEval2017/Humor/AD/500MBnew.txt ~/Developer/Thesis/SemEval2017/Humor/AD/new500MB.txt
+python3 pre_process.py ~/Developer/Thesis/SemEval2017/Humor/AD/1GBnew.txt ~/Developer/Thesis/SemEval2017/Humor/AD/new1GB.txt
+python3 pre_process.py ~/Developer/Thesis/SemEval2017/Humor/AD/6GBnew.txt ~/Developer/Thesis/SemEval2017/Humor/AD/new6GB.txt
