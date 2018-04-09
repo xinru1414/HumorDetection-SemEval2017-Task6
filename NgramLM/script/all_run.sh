4a5271262c778e861ed78fd688ffaa072401e301
Yan Xinru <yanxx418@d.umn.edu>
2018-04-08 19:52:04 -0500

    kenlm fix

--- NgramLM/script/all_run.sh
+++ NgramLM/script/all_run.sh
@@ -5,13 +5,13 @@ echo 'PreProcessing data is done'
 echo 'Now we train languge model'
 ../KenLM/kenlm-master/build/bin/lmplz -o 3 -S 70% < ../../data/news_dir/various_size_news/new1000line.txt > ../mydata/1000line.arpa
 echo 'Getting language model scores'
-pipenv run python ../src/beets.py ../../data/evaluation_dir/evaluation_data_pre/ ../mydata/lm_news_result/lm_score/ ../mydata/1000line.arpa
+python2 ../src/beets.py ../../data/evaluation_dir/evaluation_data_pre/ ../mydata/lm_news_result/lm_score/ ../mydata/1000line.arpa
 echo 'Evaluating Task A (with 1000line training data)'
-python ../../Evaluation/cilantro_a.py ../mydata/lm_news_result/lm_score/ news Ngram
-python ../../Evaluation/TaskA_Eval/TaskA_Eval_Script.py ../../NgramLM/mydata/lm_news_result/A_lm ../../data/gold_data
+python2 ../../Evaluation/cilantro_a.py ../mydata/lm_news_result/lm_score/ news Ngram
+python2 ../../Evaluation/TaskA_Eval/TaskA_Eval_Script.py ../../NgramLM/mydata/lm_news_result/A_lm ../../data/gold_data
 echo 'Evaluating Task B (with 1000line training data)'
-python ../../Evaluation/cilantro_b.py ../mydata/lm_news_result/lm_score/ news Ngram
-python ../../Evaluation/TaskB_Eval/TaskB_Eval_Script.py ../../NgramLM/mydata/lm_news_result/B_lm ../../data/gold_data
+python2 ../../Evaluation/cilantro_b.py ../mydata/lm_news_result/lm_score/ news Ngram
+python2 ../../Evaluation/TaskB_Eval/TaskB_Eval_Script.py ../../NgramLM/mydata/lm_news_result/B_lm ../../data/gold_data
 # echo '1MB'
 # pipenv run python ../src/beets.py ../../data/evaluation_dir/evaluation_data_pre/ ../mydata/lm_news_result/lm_score/ ../mydata/1MB.arpa
 # python ../../Evaluation/cilantro_a.py ../mydata/lm_news_result/lm_score/ news Ngram
